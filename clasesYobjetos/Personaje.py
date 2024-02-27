60f592c0360aa279772d9af1ebb5528355f2a648
Andrea Medina 07 <andreaaliciamedinaeverardo@gmail.com>
2024-02-26 20:29:15 -0600

    Ejemplo de encapsulamiento

--- clasesYobjetos/Personaje.py
+++ clasesYobjetos/Personaje.py
@@ -1,16 +1,20 @@
 #crear una clase llamada personaje
 class Personaje: 
     #crear atributos del personaje
-    especie="Humano"
-    nombre="John"
-    altura=2.18
+    
+    #atributo del personaje
+    #Declaramos el costructor para crear los objetos
+    def __init__(self, esp, nom, alt):
+        self.especie= esp
+        self.nombre= nom
+        self.altura= alt
     
     #crear los métodos del personaje
     def correr(self,estado):
         if(estado):
-            print("El personaje"+ self.nombre+ " está corriendo ")
+            print("El personaje "+ self.nombre+ " está corriendo ")
         else:
-            print("El personaje"+ self.nombre+ " está muerto")
+            print("El personaje "+ self.nombre+ " está muerto")
             
     #crear un segundo método del personaje el self es para llamar los atributos de la misma clase
     def lanzarGranada(self):
--- clasesYobjetos/main.py
+++ clasesYobjetos/main.py
@@ -5,19 +5,51 @@
 from Personaje import *
 from Armas import *
 
+#solicitar datos Spartan
+print("======= DATOS DEL HEROE =======")
+nombreS=input("Escribe el nombre de tu Spartan ")
+especieS=input("Escribe la especie de tu Spartan ")
+alturaS=float(input("Escribe la altura de tu Spartan "))
+print("")
+
+#solicitar datos del Nemesis
+print("======= DATOS DEL NEMESIS =======")
+nombreN=input("Escribe el nombre de tu Nemesis ")
+especieN=input("Escribe la especie de tu Nemesis ")
+alturaN= float(input("Escribe la altura de tu Nemesis "))
+print("")
+
 #crear la primer objeto de la clase del personaje
-spartan= Personaje()
+Spartan= Personaje(especieS,nombreS,alturaS)
+Nemesis= Personaje(especieN,nombreN,alturaN)
 Arma= Armas()
 
-#ya podemos utilizar los metodos y los atributos una vez creada la instancia
-print(spartan.nombre)
-print(spartan.especie)
-print(spartan.altura)
+#usamos los atributos de spartan
+print("======= EL OBJETO SPARTAN CONTIENE =======")
+print(Spartan.nombre )
+print(Spartan.especie )
+print(Spartan.altura )
+print("")
 
-#Usamos los metdos del spartan
-spartan.correr(False)
-spartan.lanzarGranada()
+#usamos los atributos de nemesis
+print("======= EL OBJETO NEMESIS CONTIENE =======")
+print(Nemesis.nombre )
+print(Nemesis.especie )
+print(Nemesis.altura )
+print("")
+
+#Usamos los metodos del spartan
+print("====== METODOS DEL OBJETO SPARTAN ======")
+Spartan.correr(False)
+Spartan.lanzarGranada()
+print("")
+
+#Usamos los metodos del Nemesis
+print("====== METODOS DEL OBJETO NEMESIS ======")
+Nemesis.correr(False)
+Nemesis.lanzarGranada()
+print("")
 
 #Usamos los metodos del arma
-Arma.seleccionarArma(spartan.nombre)
+Arma.seleccionarArma(Spartan.nombre)
 Arma.recargarArma(65)
